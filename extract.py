121336cc8f52491c2a48b14971c1a070a23a3cbd
ha4219 <ha4219@naver.com>
2022-06-05 12:21:07 +0900

    FIX format to pylint acc FIX pylint workflow #4

--- extract.py
+++ extract.py
@@ -10,24 +10,22 @@ from face_alignment import FaceAlignment, LandmarksType
 
 
 def extract_model_feature():
-	'''
+    '''
 		function
 		:parameter {None}: none
 		:return {None}: none
 	'''
-	settings = Settings()
+    settings = Settings()
+    alignment = FaceAlignment(LandmarksType._3D, flip_input=False)
 
-	alignment = FaceAlignment(LandmarksType._3D, flip_input=False)
-	print(alignment.get_landmarks)
-	
-	torch.save(alignment, os.path.join(settings.KEEPMODEL, "face_alignment.pth"))
+    torch.save(alignment, os.path.join(settings.KEEPMODEL, "face_alignment.pth"))
 
-	weight_path = "face_parsing/res/cp/79999_iter.pth"
-	parsing = BiSeNet(19)
-	parsing.load_state_dict(torch.load(weight_path, map_location='cpu'))
+    weight_path = "face_parsing/res/cp/79999_iter.pth"
+    parsing = BiSeNet(19)
+    parsing.load_state_dict(torch.load(weight_path, map_location='cpu'))
 
-	torch.save(parsing, os.path.join(settings.KEEPMODEL, 'face_parsing.pth'))
+    torch.save(parsing, os.path.join(settings.KEEPMODEL, 'face_parsing.pth'))
 
 
 if __name__ == '__main__':
-	extract_model_feature()
+    extract_model_feature()
--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 main fastapi
 '''
 import os
+import io
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 import torch
 import numpy as np
-import cv2
-import io
 from PIL import Image
 
 from config import Settings
@@ -23,95 +22,94 @@ alignment = torch.load(os.path.join(settings.KEEPMODEL, 'face_alignment.pth'))
 parsing = torch.load(os.path.join(settings.KEEPMODEL, 'face_parsing.pth')).to(settings.KEEPCUDA)
 
 def execute_alignment(img, dst: str):
-	'''
-	run alignment
-	'''
-	np.array(alignment.get_landmarks(np.array(img))).tofile(dst)
+    '''
+    run alignment
+    '''
+    np.array(alignment.get_landmarks(np.array(img))).tofile(dst)
 
 def execute_parsing(img, dst: str):
-	'''
-	run parsing
-	'''
-	print(type(img))
-	_execute_face_parsing(dst, img, parsing, settings.KEEPCUDA)
+    '''
+    run parsing
+    '''
+    _execute_face_parsing(dst, img, parsing, settings.KEEPCUDA)
 
 # TODO(ha4219): #1 FEAT post backbone function
 
 @app.get('/')
 def read_root():
-	'''
-	url / test
-	'''
-	return {"Hello": "World"}
+    '''
+    url / test
+    '''
+    return {"Hello": "World"}
 
 
 @app.post('/uploader')
 async def uploader(
-		front: UploadFile = File(None),
-		text: UploadFile = File(None),
-		back: UploadFile = File(None),
-		back_text: UploadFile = File(None),
-		style: str = Form("chram"),
-		shape: str = Form("circle"),
-		border: str = Form("basic"),
-		embo: bool = Form(False),
-		emboline: bool = Form(False),
+        front: UploadFile = File(None),
+        text: UploadFile = File(None),
+        back: UploadFile = File(None),
+        back_text: UploadFile = File(None),
+        style: str = Form("chram"),
+        shape: str = Form("circle"),
+        border: str = Form("basic"),
+        embo: bool = Form(False),
+        emboline: bool = Form(False),
 	):
-	'''
-	basic logic
+    '''
+    basic logic
 
-	Parameters
-	-----------
-		front: UploadFile
-			앞면 이미지
-		text: UploadFile
-			앞면 텍스트 파일
-		back: UploadFile
-			뒷면 이미지
-		back_text: UploadFile
-			뒷면 텍스트 파일
-		style: str
-			charm(참) - default
-			frame(프레임)
-		shape: str
-			circle(원형) - default
-			square(사각)
-			ellipse(타원)
-			octagon(팔각)
+    Parameters
+    -----------
+        front: UploadFile
+            앞면 이미지
+        text: UploadFile
+            앞면 텍스트 파일
+        back: UploadFile
+            뒷면 이미지
+        back_text: UploadFile
+            뒷면 텍스트 파일
+        style: str
+            charm(참) - default
+            frame(프레임)
+        shape: str
+            circle(원형) - default
+            square(사각)
+            ellipse(타원)
+            octagon(팔각)
 
-		border: str
-			basic(기본) - default
-			bead(구슬)
-			twist(꽈배기)
-			curve(굴곡)
+        border: str
+            basic(기본) - default
+            bead(구슬)
+            twist(꽈배기)
+            curve(굴곡)
 
-		embo: str
-			true(엠보)
-			false(민자) - default
+        embo: str
+            true(엠보)
+            false(민자) - default
 
-		emboline: str
-			엠보라인 - 기본배경*embo-false시에 추가가능
-			true(추가)
-			false(추가하지않음) - default
-	Return
-	-----------
-		res: json
-		return saved path
-	'''
-	if not front:
-		raise HTTPException(status_code=517, detail="front parameter is required.")
-	front = Image.open(io.BytesIO(await front.read()))
-	front_text = Image.open(io.BytesIO(await front_text.read())) if text else 'NONE'
-	back = Image.open(io.BytesIO(await back.read())) if back else 'NONE'
-	back_text = Image.open(io.BytesIO(await back_text.read())) if back_text else 'NONE'
+        emboline: str
+            엠보라인 - 기본배경*embo-false시에 추가가능
+            true(추가)
+            false(추가하지않음) - default
+    Return
+    -----------
+        res: json
+        return saved path
+    '''
+    if not front:
+        raise HTTPException(status_code=517, detail="front parameter is required.")
+    front = Image.open(io.BytesIO(await front.read()))
+    front_text = Image.open(io.BytesIO(await front_text.read())) if text else 'NONE'
+    back = Image.open(io.BytesIO(await back.read())) if back else 'NONE'
+    back_text = Image.open(io.BytesIO(await back_text.read())) if back_text else 'NONE'
 
-	try:
-		execute_alignment(front, "test.bin")
-	except Exception as exc:
-		raise HTTPException(status_code=518, detail="alignment error") from exc
-	try:
-		execute_parsing(front, "test.png")
-	except Exception as exc:
-		raise HTTPException(status_code=519, detail="parsing error") from exc
+    try:
+        execute_alignment(front, "test.bin")
+    except Exception as exc:
+        raise HTTPException(status_code=518, detail="alignment error") from exc
+    try:
+        execute_parsing(front, "test.png")
+    except Exception as exc:
+        raise HTTPException(status_code=519, detail="parsing error") from exc
 
-	return {'k': 'h'}
+    return {'k': 'h'}
